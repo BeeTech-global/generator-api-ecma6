7ad4ec717ae0f90b8a31ab86ef9f4ddc1e581d20
Guilherme Moreira <321.guilherme@gmail.com>
2017-04-05 15:56:29 -0300

    knex and knex conf added

---
+++ src/common/knexConf.js
@@ -0,0 +1,22 @@
+var db_conf = require('../../database.json');
+var env = process.env.NODE_ENV || 'dev';
+var knex_conf = {
+  client: db_conf[env].driver,
+  connection: {
+    filename: db_conf[env].filename
+  }
+}
+
+if(db_conf[env].driver == 'pg') {
+  knex_conf = {
+    client: db_conf[env].driver,
+    connection: {
+      host : db_conf[env].host,
+      user : db_conf[env].user,
+      password : db_conf[env].password,
+      database : db_conf[env].database
+    }
+  }
+}
+
+module.exports = knex_conf;
