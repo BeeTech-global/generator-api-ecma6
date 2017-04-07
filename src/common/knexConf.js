60fe1fbd9dfc6035d6e0972f1afc6e2413a76195
Guilherme Moreira <321.guilherme@gmail.com>
2017-04-07 11:24:35 -0300

    knexconf lint update

--- src/common/knexConf.js
+++ src/common/knexConf.js
@@ -1,20 +1,20 @@
-var db_conf = require('../../database.json');
-var env = process.env.NODE_ENV == 'test' ? 'development' : 'default';
-var knex_conf = {
+let db_conf = require('../../database.json');
+let env = process.env.NODE_ENV === 'test' ? 'development' : 'default';
+let knex_conf = {
   client: db_conf[env].driver,
   connection: {
     filename: db_conf[env].filename
   }
 }
 
-if(db_conf[env].driver == 'pg') {
+if(db_conf[env].driver === 'pg') {
   knex_conf = {
     client: db_conf[env].driver,
     connection: {
-      host : db_conf[env].host,
-      user : db_conf[env].user,
-      password : db_conf[env].password,
-      database : db_conf[env].database
+      host: db_conf[env].host,
+      user: db_conf[env].user,
+      password: db_conf[env].password,
+      database: db_conf[env].database
     }
   }
 }
