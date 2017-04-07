33a5a648d67121cbb9874d477fd2d5abb8fe123e
Guilherme Moreira <321.guilherme@gmail.com>
2017-04-07 17:21:23 -0300

    db integration update

--- src/core/BaseService.js
+++ src/core/BaseService.js
@@ -1,28 +1,15 @@
 const EventEmitter = require('events');
 const Promise = require('bluebird');
-const sting = require('@beetech/bee-sting')
 
 module.exports = class BaseService extends EventEmitter {
-  constructor(repository, schema) {
+  constructor(repository) {
     super();
     this.repository = repository;
-    this.schema = schema;
     this.Promise = Promise;
-    this.sting = sting;
   }
 
   post(obj) {
-    return new this.Promise((resolve, reject) => {
-      sting.default.validate(obj, this.schema).then(
-        (office) => {
-          this.repository.add(office).then(
-            res => resolve(res),
-            err => reject(err)
-          )
-        },
-        err => reject(err)
-      );
-    });
+    return this.repository.add(obj);
   }
 
   get() {
